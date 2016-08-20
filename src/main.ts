9aef2a24f3013ea208cb133fdd5381dff9ee5fbc
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-08-20 14:34:15 +0100

    BNC: Updated inport to angular

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-var angular = require("angular");
+/// <reference path="../typings/globals/jquery/index.d.ts" />
+/// <reference path="../typings/globals/angular/index.d.ts" />
+import angular = require("angular");
 import { HomeController } from "./home.controller/home.controller";
 import { RoomController } from "./room.controller/room.controller";
 import { SocketService } from "./socket.service/socket.service";
@@ -7,9 +9,9 @@ export module Bnc {
     "use strict";
 
     var app = angular.module("app", ["ngSanitize", "ngRoute"])
-                    .controller("home.controller", HomeController)
-                    .controller("room.controller", RoomController)
-                    .controller("home.controller", HomeController)
-                    .factory("socket.service", SocketService);
+        .controller("home.controller", HomeController)
+        .controller("room.controller", RoomController)
+        .controller("home.controller", HomeController)
+        .factory("socket.service", SocketService);
     export var Application = app;
 }
