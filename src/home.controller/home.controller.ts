4cf630b080e555e5d5749ed2238cf886e8ba5cd3
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-08-20 23:51:54 +0100

    BNC: Added submitRoom method in home controller

--- src/home.controller/home.controller.ts
+++ src/home.controller/home.controller.ts
@@ -2,12 +2,20 @@ export class HomeController {
     "use strict";
 
     constructor(private $scope, private $log: any, private $location: any) {
+        $scope.room;
         $scope.createRoom = this.createRoom;
+        $scope.submitRoom = this.submitRoom;
         $scope.message = "Hello world!";
     }
 
     createRoom = () => {
         console.log("Clicked room");
         this.$location.path("/room/1");
+    };
+
+    submitRoom = (form) => {
+        if(!form.invalid) {
+            this.$location.path("/room/" + this.$scope.room);
+        }
     };
 }
