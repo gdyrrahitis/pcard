3eb1f8f5e4cd1b11f5536254c8c7e1b91e1ff7ac
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-08-20 14:51:25 +0100

    BNC: Wrapped configuration around an RouteConfig method

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,21 @@
-import { Bnc } from "./main";
-var app = Bnc.Application;
+export module Routes {
+    export function RouteConfig(app: any) {
+        "use strict";
 
-app.config(($routeProvider) => {
-    $routeProvider.when("/", {
-        templateUrl: "home.controller/home.controller.html",
-        controller: "home.controller/home.controller.js"
-    })
-    .when("/room/:id", {
-        templateUrl: "room.controller/room.controller.html",
-        controller: "room.controller/room.controller.js"
-    })
-    .otherwise({
-        redirectTo: "/"
-    });
-});
+        app.config(($routeProvider, $locationProvider) => {
+            $locationProvider.html5Mode(true);
+
+            $routeProvider.when("/", {
+                templateUrl: "home.controller/home.controller.html",
+                controller: "home.controller"
+            })
+            .when("/room/:id", {
+                templateUrl: "room.controller/room.controller.html",
+                controller: "room.controller"
+            })
+            .otherwise({
+                redirectTo: "/"
+            });
+        });
+    }
+}
