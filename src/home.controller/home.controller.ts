e96490bd865bfdf95960b74ee88b9733d23dcdbb
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-08-20 22:27:49 +0100

    BNC: Added boilerplate code for home controller

--- src/home.controller/home.controller.ts
+++ src/home.controller/home.controller.ts
@@ -1,3 +1,13 @@
 export class HomeController {
     "use strict";
+
+    constructor(private $scope, private $log: any, private $location: any) {
+        $scope.createRoom = this.createRoom;
+        $scope.message = "Hello world!";
+    }
+
+    createRoom = () => {
+        console.log("Clicked room");
+        this.$location.path("/room/1");
+    };
 }
