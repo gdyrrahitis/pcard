0804400a4cba95531e6757f1e297e378004fd479
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-08-20 13:17:10 +0100

    BNC: Added module initializaton in main.ts

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+var angular = require("angular");
+import { HomeController } from "./home.controller/home.controller";
+import { RoomController } from "./room.controller/room.controller";
+import { SocketService } from "./socket.service/socket.service";
+
+export module Bnc {
+    "use strict";
+
+    var app = angular.module("app", ["ngSanitize", "ngRoute"])
+                    .controller("home.controller", HomeController)
+                    .controller("room.controller", RoomController)
+                    .controller("home.controller", HomeController)
+                    .factory("socket.service", SocketService);
+    export var Application = app;
+}
