efbafd553a7404d7cb42c5a41461a33ed85fd17c
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-08-20 14:50:20 +0100

    BNC: Added mapping to ngSanitize and ngRoute

--- systemjs-config.js
+++ systemjs-config.js
@@ -1,12 +1,16 @@
 (function () {
     var map = {
         "bnc": "src",
-        "angular": "node_modules/angular"
+        "angular": "node_modules/angular",
+        "ngSanitize": "node_modules/angular-sanitize",
+        "ngRoute": "node_modules/angular-route"
     };
 
     var packages = {
         "bnc": { main: "main.js", defaultExtension: "js" },
-        "angular": { main: "index.js", defaultExtension: "js" }
+        "angular": { main: "index.js", defaultExtension: "js" },
+        "ngSanitize": { main: "index.js", defaultExtension: "js" },
+        "ngRoute": { main: "index.js", defaultExtension: "js" }
     };
 
     var config = {
