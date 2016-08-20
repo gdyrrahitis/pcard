71a9ed55aa9b13852ab35a15d7a89282c7bccec1
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-08-20 23:50:22 +0100

    BNC: Added additional tasks to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,44 @@
-(function () {
+(function() {
     var gulp = require("gulp"),
         sass = require("gulp-sass"),
+        express = require("gulp-express"),
+        concat = require("gulp-concat"),
+        uglify = require("gulp-uglify"),
         merge = require("merge-stream"),
+        sequence = require("run-sequence"),
         del = require("del"),
         browserSync = require("browser-sync"),
         reload = browserSync.reload;
 
     // JavaScript tasks
+    gulp.task("js:copy", function() {
+        gulp.src("node_modules/systemjs/dist/system.src.js")
+            .pipe(gulp.dest("dist/js/libs"));
+    });
+
+    gulp.task("js", ["js:copy"]);
 
     // Css tasks
+    gulp.task("css:min", function() {
+        gulp.src("dist/css/**/*.css")
+            .pipe(uglify())
+            .pipe(gulp.dest("."));
+    });
+
+    gulp.task("css", ["css:uglify"]);
 
     // SASS tasks
-    gulp.task("sass", ["fonts"], function () {
+    gulp.task("sass", ["fonts"], function() {
         var custom = gulp.src("src/**/*.scss")
             .pipe(sass())
             .pipe(gulp.dest("dist/css"))
+            .pipe(reload({
+                stream: true
+            }));
+
+        var customFontAwesome = gulp.src("src/styles/custom-font-awesome.scss")
+            .pipe(sass())
+            .pipe(gulp.dest("dist/css/libs"))
             .pipe(reload({
                 stream: true
             }));
@@ -32,11 +56,12 @@
             .pipe(sass())
             .pipe(gulp.dest("dist/css/libs"));
 
-        return merge(custom, bootstrap, fontAwesome);
+
+        return merge(custom, customFontAwesome, bootstrap, fontAwesome);
     });
 
     // Fonts
-    gulp.task("fonts", function () {
+    gulp.task("fonts", function() {
         var bootstrap = gulp.src("node_modules/bootstrap-sass/assets/fonts/bootstrap/*.*")
             .pipe(gulp.dest("dist/css/fonts/bootstrap"));
 
@@ -47,23 +72,33 @@
     });
 
     // Clean
-    gulp.task("clean", function () {
+    gulp.task("clean", function() {
         return del("dist/**");
     });
 
     // Watching
-    gulp.task("watch", ["sass"], function () {
+    gulp.task("watch", ["build"], function() {
         gulp.watch("src/**/*.scss", ["sass"]);
+        gulp.watch("src/**/*.html", reload);
+        gulp.watch("src/**/*.js", reload);
+        gulp.watch("index.html", reload);
     });
 
     // Browsersync
-    gulp.task("browser:sync", function () {
+    gulp.task("browser:sync", function() {
         browserSync.init(null, {
             proxy: "http://localhost:54879"
         })
     });
 
     // Build
-    gulp.task("build", ["sass"]);
-    gulp.task("build:w", ["browser:sync", "watch"]);
+    gulp.task("build", ["sass", "js", "fonts"]);
+    gulp.task("build:w", function(callback) {
+        sequence("clean", ["browser:sync", "watch"], callback);
+    });
+
+    // Start server
+    gulp.task("server", ["build:w"], function() {
+        express.run(["server.js"]);
+    });
 })();
