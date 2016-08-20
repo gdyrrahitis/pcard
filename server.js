5d18bceaac6f403aa9730b7a3fea7f2474347565
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-08-20 22:26:59 +0100

    BNC: Added catch-all rule to express server

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ var express = require("express"),
 
 app.use(express.static("src"));
 app.use(express.static("node_modules"));
+app.use(express.static("dist"));
 app.use(express.static(__dirname));
+
+app.all('/*', function(req, res, next) {
+    // Just send the index.html for other files to support HTML5Mode
+    res.sendFile('index.html', { root: __dirname });
+});
 
 server.listen(port, function () {
     console.log("Server is up and running, listening on port: " + port);
