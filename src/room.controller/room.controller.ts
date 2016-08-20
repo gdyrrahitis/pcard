047124665dd20f12e6c2c0007a8045a1c8ebd7d2
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-08-20 22:29:26 +0100

    BNC: Added boilerplate code in room controller

--- src/room.controller/room.controller.ts
+++ src/room.controller/room.controller.ts
@@ -1,3 +1,17 @@
 export class RoomController {
     "use strict";
+    private mountainGoat = ["0", "1/2", "1", "2", "3", "5", "8", "13", "20", "40", "100", "cup", "?"];
+
+    constructor(private $scope: any, private $log: any) {
+        $scope.list = this.mountainGoat;
+        $scope.selectedItem;
+        $scope.selectedList = [];
+        $scope.selectCard = this.selectCard;
+    }
+
+    selectCard = (element) => {
+        this.$log.info(element);
+        this.$scope.selectedItem = element.card;
+        this.$scope.selectedList.push(this.$scope.selectedItem);
+    };
 }
