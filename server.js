a0126f194368fbfbde0a07fb7c9b66354e9bc92b
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-08-21 21:50:03 +0100

    BNC: Added private room functionality

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-var express = require("express"),
-    path = require("path"),
-    app = express(),
-    server = require("http").Server(app)
-    io = require("socket.io")(server),
-    port = 54879;
+var express = require("express");
+var path = require("path");
+var app = express();
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+var port = 54879;
+var rooms = [];
+var maxRoomsAllowed = 1;
 
 app.use(express.static("src"));
 app.use(express.static("node_modules"));
@@ -16,15 +18,73 @@ app.all('/*', function(req, res, next) {
 });
 
 // Socket.io configuration
-io.on("connection", function (socket) {  
-    console.log("New connection made to server.");
+io.on("connection", function(socket) {
+    console.log("New connection made to server." + socket.id);
 
-    // Join room
-    socket.on("join", function (data) {  
+    // Disconnect socket
+    socket.on("disconnect", function() {
+
+    });
+
+    // // Join room
+    // socket.on("join", function(data) {
+    //     var filtered = rooms.filter(function(r) {
+    //         return r.room == data.room;
+    //     });
+
+    //     // if (filtered.length == 0) {
+    //     // if(rooms.length >= maxRoomsAllowed) {
+    //     //     io.to(socket.id).emit("all-rooms-occupied");
+    //     //     return;
+    //     // }
+    //     console.log(rooms);
+    //     rooms[data.room] = socket;
+    //     console.log(socket);
+    //     // No room found, book it
+    //     var roomObj = {
+    //         id: socket.id,
+    //         room: data.room
+    //     };
+    //     rooms.push(roomObj);
+    //     // io.to(socket.id).emit("room-access-granted", roomObj);
+    //     // return;
+    //     // }
+
+    //     // // Room found, do nothing
+    //     // io.to(socket.id).emit("room-occupied");
+    // });
+
+    socket.on("join-private", function(data) {
+
+        socket.room = "room-" + data.room;
+        rooms[socket.room] = socket;
+        // No room found, book it
+        var roomObj = {
+            id: socket.id,
+            userId: data.userId,
+            room: data.room
+        };
+        rooms.push(roomObj);
+        socket.join("private-" + data.room);
+
+        console.log("Joined private room: " + data.room);
+        io.to("private-" + data.room).emit("show-attendees", rooms);
+    });
+
+    socket.on("private-leave", function(data) {
         console.log(data);
+        var room = rooms.filter(function(r) {
+            return r.userId === data.id;
+        });
+        console.log(room);
+        rooms = rooms.filter(function(r) {
+            return r.userId !== data.id;
+        });
+
+        io.to("private-" + room.room).emit("show-attendees", rooms);
     });
 });
 
-server.listen(port, function () {
+server.listen(port, function() {
     console.log("Server is up and running, listening on port: " + port);
 });
