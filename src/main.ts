3f537d833648f04e09da5a8f2296a1e2eb8abe9f
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-08-21 21:50:40 +0100

    BNC: Added menu controller and ngStorage in app module

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,21 @@
 import angular = require("angular");
 import "ngSanitize";
 import "ngRoute";
+import "ngStorage";
 import { HomeController } from "./home.controller/home.controller";
 import { RoomController } from "./room.controller/room.controller";
+import { MenuController } from "./menu.controller/menu.controller";
 import { SocketService } from "./socket.service/socket.service";
 import { Routes } from "./routes";
 
 export module Bnc {
     "use strict";
 
-    var app = angular.module("app", ["ngSanitize", "ngRoute"])
+    var app = angular.module("app", ["ngSanitize", "ngRoute", "ngStorage"])
         .controller("homeController", HomeController)
         .controller("roomController", RoomController)
-        .factory("socketService", [SocketService]);
+        .controller("menuController", MenuController)
+        .factory("socketService", ["$rootScope", SocketService]);
 
     // Configure routes
     Routes.RouteConfig(app);
