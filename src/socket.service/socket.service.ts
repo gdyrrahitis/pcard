34dbc1d118e7cf9487850746f3246fa31e648ad5
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-08-21 16:57:20 +0100

    BNC: Implemented basic on, emit functions for socket service

--- src/socket.service/socket.service.ts
+++ src/socket.service/socket.service.ts
@@ -1,3 +1,33 @@
+import io = require('socket.io-client');
+
 export class SocketService {
     "use strict";
+    
+    private socket: any;
+    private data: any;
+
+    constructor(private $rootScope: any) {
+        this.socket = io.connect("http://localhost:54879");
+    }
+
+    on(eventName, callback) {
+        this.socket.on(eventName, () => {
+            var args = arguments;
+            this.$rootScope.$apply(() => {
+                callback.apply(this.socket, args);
+            });
+        });
+    }
+
+    emit(eventName, data, callback) {
+        this.socket.emit(eventName, data, () => {
+            var args = arguments;
+            this.$rootScope.$apply(() => {
+                if (callback) {
+                    callback.apply(this.socket, args);
+                }
+            });
+        });
+    }
+
 }
