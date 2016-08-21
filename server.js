791e68354b2c8eea9c4c92ab7ecccde97d1e13fe
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-08-21 16:53:59 +0100

    BNC: Added connection to socket.io server

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require("express"),
     path = require("path"),
     app = express(),
-    server = require("http").Server(app),
+    server = require("http").Server(app)
+    io = require("socket.io")(server),
     port = 54879;
 
 app.use(express.static("src"));
@@ -12,6 +13,16 @@ app.use(express.static(__dirname));
 app.all('/*', function(req, res, next) {
     // Just send the index.html for other files to support HTML5Mode
     res.sendFile('index.html', { root: __dirname });
+});
+
+// Socket.io configuration
+io.on("connection", function (socket) {  
+    console.log("New connection made to server.");
+
+    // Join room
+    socket.on("join", function (data) {  
+        console.log(data);
+    });
 });
 
 server.listen(port, function () {
