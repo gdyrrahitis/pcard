3f8db2060a579eda6dbe9e6da9984e1bcb7adb8d
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-08-21 21:50:17 +0100

    BNC: Added ngStorage package references

--- systemjs-config.js
+++ systemjs-config.js
@@ -4,14 +4,16 @@
         "socket.io-client": "/socket.io/socket.io.js",
         "angular": "node_modules/angular",
         "ngSanitize": "node_modules/angular-sanitize",
-        "ngRoute": "node_modules/angular-route"
+        "ngRoute": "node_modules/angular-route",
+        "ngStorage": "node_modules/ngstorage/"
     };
 
     var packages = {
         "bnc": { main: "main.js", defaultExtension: "js" },
         "angular": { main: "index.js", defaultExtension: "js" },
         "ngSanitize": { main: "index.js", defaultExtension: "js" },
-        "ngRoute": { main: "index.js", defaultExtension: "js" }
+        "ngRoute": { main: "index.js", defaultExtension: "js" },
+        "ngStorage": { main: "ngStorage.min.js", defaultExtension: "js" }
     };
 
     var config = {
