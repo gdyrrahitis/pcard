3f604f206fb505a5fa1136906b58a505984a2e1c
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-08-21 21:52:47 +0100

    BNC: Refactored room controller to show attendees

--- src/room.controller/room.controller.ts
+++ src/room.controller/room.controller.ts
@@ -1,14 +1,23 @@
+import {SocketService} from "../socket.service/socket.service";
+
 export class RoomController {
     "use strict";
-    
+
     private mountainGoat = ["zero", "half", "one", "two", "three", "five", "eight", "thirteen", "twenty", "forty", "one-hundred", "coffee", "question"];
 
-    constructor(private $scope: any, private $log: any, $routeParams) {
+    constructor(private $scope: any,
+        private $log: any,
+        private $routeParams: any,
+        private $localStorage: any,
+        private socketService: SocketService) {
         $scope.room = $routeParams.id;
         $scope.list = this.mountainGoat;
         $scope.selectedItem;
         $scope.selectedList = [];
         $scope.selectCard = this.selectCard;
+
+        this.$localStorage.id = this.socketService.getId();
+        this.socketService.on("show-attendees", this.showAttendees);
     }
 
     selectCard = (element) => {
@@ -16,4 +25,9 @@ export class RoomController {
         this.$scope.selectedItem = element.card;
         this.$scope.selectedList.push(this.$scope.selectedItem);
     };
+
+    private showAttendees(data) {
+        console.log("Showing attendees");
+        console.log(data);
+    }
 }
