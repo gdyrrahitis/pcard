88bf1ecb683549a35a16260889e21aab87983e0a
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-08-28 20:15:43 +0100

    BNC: Aded handlers for room creation or join

--- src/home.controller/home.controller.ts
+++ src/home.controller/home.controller.ts
@@ -3,47 +3,59 @@ import { SocketService } from "../socket.service/socket.service";
 export class HomeController {
     "use strict";
 
-    constructor(private $scope, 
+    constructor(private $scope,
         private $log: any,
         private $location: any,
         private $localStorage: any,
         private socketService: SocketService) {
         $scope.clickedCreate = false;
-        $scope.errorSingle;
-        $scope.errorAll;
+        $scope.error;
         $scope.createRoom = this.createRoom;
         $scope.submitRoom = this.submitRoom;
 
         this.socketService.on("room-access-granted", this.accessGranted);
         this.socketService.on("room-occupied", this.accessDenied);
         this.socketService.on("all-rooms-occupied", this.allRoomsOccupied);
+        this.socketService.on("room-not-found", this.roomNotFound);
 
         var id = this.socketService.getId();
-        if(id) {
+        if (id) {
             this.socketService.emit("disconnect", id);
         }
     }
 
     createRoom = () => {
+        let that = this;
         let room = 1;
-        this.join(room);
-        this.$location.path(`/room/${room}`);
+        this.join("create-private", room, (data) => {
+            if (data.access) {
+                that.$location.path(`/room/${room}`);
+            }
+        });
     };
 
     submitRoom = (form) => {
+        let that = this;
         if (form.$valid) {
-            this.join(this.$scope.room);
-            this.$location.path(`/room/${this.$scope.room}`);
+            this.join("join-private", this.$scope.room, (data) => {
+                if (data.access) {
+                    that.$location.path(`/room/${that.$scope.room}`);
+                }
+            });
         }
 
         // TODO: Print errors
     };
- 
-    private join = (room: number) => {
-        this.socketService.emit("join-private", {
+
+    private roomNotFound = () => {
+        this.$scope.error = "Could not find room.";
+    };
+
+    private join = (event: string, room: number, callback?: any) => {
+        this.socketService.emit(event, {
             userId: this.socketService.getId(),
             room: room
-        });
+        }, callback);
     };
 
     private accessGranted = (data) => {
@@ -51,20 +63,10 @@ export class HomeController {
     }
 
     private accessDenied = () => {
-        this.$scope.errorSingle = {
-            singleRoom: {
-                msg: "Cannot access room because it is already booked!"
-            }
-        };
-        this.$log.error(this.$scope.errorSingle.singleRoom.msg);
+        this.$scope.error = "Cannot access room because it is already booked!";
     }
 
     private allRoomsOccupied = () => {
-        this.$scope.errorAll = {
-            allRooms: {
-                msg: "All rooms are busy. Try again later!"
-            }
-        };
-        this.$log.error(this.$scope.errorAll.allRooms.msg);
+        this.$scope.error = "All rooms are busy. Try again later!";
     }
 }
