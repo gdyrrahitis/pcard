34cee8d22d983723fd5691ba33078baa3bd32b65
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-09-03 21:30:36 +0100

    BNC: Added new server.ts file to initialize the http server with socket.io

---
+++ server/server.ts
@@ -0,0 +1,16 @@
+import * as app from "../app";
+
+import http = require("http");
+import { Socket } from "./socket.io/socket.configuration";
+import socketIo = require("socket.io");
+
+let server = http.createServer(<any>app);
+let port: number = 54879;
+
+let io = socketIo(server);
+let socket = new Socket(io);
+socket.connect();
+
+server.listen(port, () => {
+    console.log("Server is up and running, listening on port: " + port);
+});
