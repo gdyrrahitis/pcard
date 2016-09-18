220ff2f33893f6cfd8fa7b6e6d3c4f853ef2d9c8
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-09-18 19:43:01 +0100

    BNC: Reorganised code

--- server/server.ts
+++ server/server.ts
@@ -1,18 +1,12 @@
 var config: ServerAppConfig.ServerConfiguration = require("./server.config.json");
-
 import * as app from "../app";
-
 import http = require("http");
-import { Socket } from "./socket.io/socket.configuration";
-import * as socketIo from "socket.io";
+import { registerSocketIo } from "./setup.socket";
 
-let server = http.createServer(<any>app);
 let port: number = 54879;
+let server = http.createServer(<any>app);
 
-let io = socketIo(server);
-let socket = new Socket(io);
-socket.connect();
-
-server.listen(port, () => {
-    console.log("Server is up and running, listening on port: " + port);
-});
+registerSocketIo(server).
+    listen(port, () => {
+        console.log("Server is up and running, listening on port: " + port);
+    });
