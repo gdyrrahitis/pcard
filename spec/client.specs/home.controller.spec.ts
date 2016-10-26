db304b981bab8e2e3214b0d769effed98142838e
George Dyrrachitis <GeorgeD@cubictelecom.com>
2016-10-26 17:56:45 +0100

    BNC: Updated specs

---
+++ spec/client.specs/home.controller.spec.ts
@@ -0,0 +1,23 @@
+// import { HomeController } from "../../src/home.controller/home.controller";
+
+// describe("HomeController spec", () => {
+
+//     let controller: HomeController;
+//     let $scope: IHomeControllerScope;
+
+//     beforeEach(angular.mock.module("app"));
+//     beforeEach(angular.mock.inject(function($rootScope, $controller) {
+//         console.log($rootScope);
+//         console.log($controller);
+//         $scope = <IHomeControllerScope>$rootScope.new();
+//         controller = $controller("homeController", {
+//             $scope: $scope
+//         });
+//     }));
+
+//     it("should create a HomeController instance", () => {
+//         // Assert
+//         console.log($scope);
+//         expect($scope).not.toBeDefined();
+//     });
+// });
