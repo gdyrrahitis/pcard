124d0449cd18575202ccc18d4a3d86d96472f681
George Dyrrachitis <GeorgeD@cubictelecom.com>
2016-10-26 12:07:22 +0100

    BNC: Added specs for base controller

--- spec/client.specs/base.controller.spec.ts
+++ spec/client.specs/base.controller.spec.ts
@@ -1,27 +1,29 @@
 import { BaseController } from "../../src/base.controller/base.controller";
 
 describe("Base controller spec", () => {
-    var scope,
-        controller;
-    var x;
-    beforeEach(angular.mock.module("app"));
-    beforeEach(inject(function ($injector) {
-        x = $injector;
-    }));
+    let $scope;
 
-    xit("Should create a new instance", () => {
+    beforeEach(() => {
+        $scope = {};
+    });
+
+    it("Should create a new instance", () => {
+        // Arrange | Act
+        let controller = new BaseController($scope);
+
         // Assert
         expect(controller).toBeDefined();
     });
 
-    xit("Should have controllerId as the input of setUniqueId", () => {
+    it("Should have controllerId as the input of setUniqueId", () => {
         // Arrange
         let input = "myUniqueId";
+        let controller = new BaseController($scope);
 
         // Act
         controller.setUniqueId(input);
 
         // Assert
-        expect(scope.controllerId).toBe(input);
+        expect($scope.controllerId).toBe(input);
     });
 });
