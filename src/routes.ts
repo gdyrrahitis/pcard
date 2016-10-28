78d456ccb7104612b9f43ec1edf334ed5874f697
George Dyrrachitis <GeorgeD@cubictelecom.com>
2016-10-28 12:46:25 +0100

    BNC: Fixed import for json

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 export function registerRoutes(app: ng.IModule, routes: Predicate<ClientAppConfig.Route>[], defaultRoute?: string) {
     "use strict";
-    var config: ClientAppConfig.ClientConfiguration = require("../client.config.json!json");
+    var config: ClientAppConfig.ClientConfiguration = require("./client.config.json");
 
     app.config(($routeProvider: ng.route.IRouteProvider, $locationProvider: ng.ILocationProvider) => {
         $locationProvider.html5Mode(config.client.html5Mode);
