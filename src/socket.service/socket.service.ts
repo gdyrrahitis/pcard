7cae2a5e275f5d1a76a15a3298f5d9e63057cd2b
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-10-31 20:06:47 +0000

    BNC: Refactored socket service

--- src/socket.service/socket.service.ts
+++ src/socket.service/socket.service.ts
@@ -3,35 +3,31 @@ export class SocketService {
 
     private data: any;
 
-    constructor(private $rootScope: ng.IScope, private socket: SocketIOClient.Socket) { }
-
-    getId() {
+    get socketId() {
         return this.socket.id;
     }
+
+    constructor(private $rootScope: ng.IScope, private socket: SocketIOClient.Socket) { }
 
     on(eventName, callback) {
         this.socket.on(eventName, (...args) =>
             this.applyOnRootScope(callback, args));
     }
 
-    private applyOnRootScope = (callback, args) => {
-        return this.$rootScope.$apply(this.actOnCallback(callback, args));
-    }
+    private applyOnRootScope = (callback, args) =>
+        this.$rootScope.$apply(this.actOnCallback(callback, args));
 
-    private actOnCallback = (callback, args) => {
-        return this.actOnCallbackIfItIsDefined(callback, args);
-    }
+    private actOnCallback = (callback, args) =>
+        this.actOnCallbackIfItIsDefined(callback, args);
 
-    private actOnCallbackIfItIsDefined = (callback, args) => {
-        return callback ? callback.apply(this.socket, args) : undefined;
-    }
+    private actOnCallbackIfItIsDefined = (callback, args) =>
+        callback ? callback.apply(this.socket, args) : undefined;
 
     emit(eventName, data?, callback?) {
         this.socket.emit(eventName, data, (...args) =>
             this.applyOnRootScopeOnDefinedCallback(callback, args));
     }
 
-    private applyOnRootScopeOnDefinedCallback = (callback, args) => {
-        return this.$rootScope.$apply(this.actOnCallbackIfItIsDefined(callback, args));
-    }
+    private applyOnRootScopeOnDefinedCallback = (callback, args) =>
+        this.$rootScope.$apply(this.actOnCallbackIfItIsDefined(callback, args));
 }
