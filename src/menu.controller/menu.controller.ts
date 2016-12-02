b441921cf70a08ba1f441bc632a4ecb18ed379e4
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2016-12-02 20:55:22 +0000

    BNC: Refactored menu.controller

--- src/menu.controller/menu.controller.ts
+++ src/menu.controller/menu.controller.ts
@@ -1,25 +1,37 @@
-import {SocketService} from "../socket.service/socket.service";
+import { SocketService } from "../socket.service/socket.service";
 import { BaseController } from "../base.controller/base.controller";
 
 export class MenuController extends BaseController {
     "use strict";
-    
+    private privateLeaveEvent: string = "private-leave";
+    private root: string = "/";
+    private _id: string;
+
     constructor(protected $scope: IMenuControllerScope,
         private $location: ng.ILocationService,
         private $localStorage: ILocalStorage,
         private socketService: SocketService
     ) {
         super($scope);
+
         this.setUniqueId("MenuController");
+        this._id = this.$localStorage.id;
 
         this.$scope.navigateToHome = this.navigateToHome;
     }
 
     navigateToHome = () => {
-        var id = this.$localStorage.id;
-        this.socketService.emit("private-leave", {
-            id: id
-        });
-        this.$location.path("/");
+        this.broadcastLeave();
+        this.$location.path(this.root);
     };
+
+    private broadcastLeave() {
+        this.socketService.emit(this.privateLeaveEvent, this.eventArguments);
+    }
+
+    private get eventArguments() {
+        return {
+            id: this._id
+        };
+    }
 }
