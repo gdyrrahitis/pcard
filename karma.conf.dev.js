15100afb762c388f53e9822389d63b1434476891
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2017-06-25 14:41:33 +0100

    Removed logLevel property

--- karma.conf.dev.js
+++ karma.conf.dev.js
@@ -1,7 +1,6 @@
 require('babel-register');
 
 var conf = require("./karma.configuration");
-conf.logLevel = "LOG_DEBUG";
 conf.autoWatch = true;
 conf.browsers = ["Chrome"];
 conf.singleRun = false;
